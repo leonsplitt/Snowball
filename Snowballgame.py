9cc81fc5d561e82893ecf18880cac06ab72c282d
Leon Splitt <splittleon@gmail.com>
2023-03-07 21:06:47 +0100

    Added background elements

--- Snowballgame.py
+++ Snowballgame.py
@@ -2,26 +2,31 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1080,720))
+screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("Leon's Game")
 clock = pygame.time.Clock()
 
-#plaintest_surface = pygame.Surface((800,600))
-#plaintest_surface.fill('Red')
-logo_surface = pygame.image.load('assets/logo.png')
-logo_x_pos = 200
+background = pygame.image.load('assets/Background.png')
+ground = pygame.image.load('assets/Ground.png')
+
+box = pygame.Surface((50,50))
+box.fill('Red')
+
+#logo_surface = pygame.image.load('assets/logo.png')
+#logo_x_pos = 200
+
+screen.blit(background, (0,0))
+screen.blit(box,(500,400))
+screen.blit(ground, (0,630))
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    logo_x_pos += 4
-    if logo_x_pos < 100: logo_x_pos = 600
-    if logo_x_pos > 700: logo_x_pos = 100
-    #screen.blit(plaintest_surface,(0,0))
-    screen.blit(logo_surface,(logo_x_pos,200))
-    # somehow moves weierdly right now
+
+    #screen.blit(logo_surface,(logo_x_pos,200))
     
 
     pygame.display.update()
