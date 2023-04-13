1387c34cd87201524cbf5abdaafb963ad85c9f96
Leon Splitt <splittleon@gmail.com>
2023-04-13 20:00:18 +0200

    Added snowball asset

--- Snowballgame.py
+++ Snowballgame.py
@@ -1,37 +1,64 @@
 import pygame
+from pygame.locals import *
 from sys import exit
 
+# constant variables
+SCREEN_SIZE = (1280,720)
+
+# init
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Leon's Game")
 clock = pygame.time.Clock()
 
+# assets
 background = pygame.image.load('assets/Background.png')
 ground = pygame.image.load('assets/Ground.png')
 
-box = pygame.Surface((50,50))
-box.fill('Red')
-
-#logo_surface = pygame.image.load('assets/logo.png')
-#logo_x_pos = 200
-
-screen.blit(background, (0,0))
-screen.blit(box,(500,400))
-screen.blit(ground, (0,630))
+# player
+class Player():
+    def __init__(self, x, y):
+        logo_surface = pygame.image.load('assets/logo.png')
+        self.image = pygame.transform.scale(logo_surface, (player_x,player_y))
 
 
+# logo_surface = pygame.image.load('assets/logo.png')
+player_x = 300
+player_y = 300
+
+
+# game loop
 while True:
+    # INPUT
+
+    # player input
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player_x -= 2
+    if keys[pygame.K_d]:
+        player_x += 2
+
+    # update
+    pygame.display.update()
+    clock.tick(60)
+
+    #draw
+    screen.blit(background, (0,0))
+    screen.blit(ground, (0,630))
+    screen.blit(logo_surface,(player_x,player_y))
+
+
+
+
+
+    #quit & frame things
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    #screen.blit(logo_surface,(logo_x_pos,200))
     
-
-    pygame.display.update()
-    clock.tick(60)
-
 
 
 
