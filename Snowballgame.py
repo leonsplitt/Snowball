114417df19c67d5c111e4a8fea8cb63caae2d8ee
Leon Splitt <splittleon@gmail.com>
2023-04-16 04:15:36 +0200

    Update Snowballgame.py

--- Snowballgame.py
+++ Snowballgame.py
@@ -12,6 +12,10 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Leon's Game")
 clock = pygame.time.Clock()
 
+logo_surface = pygame.image.load('assets/logo.png')
+player_x = 300
+player_y = 300
+
 # assets
 background = pygame.image.load('assets/Background.png')
 ground = pygame.image.load('assets/Ground.png')
@@ -20,44 +24,75 @@ ground = pygame.image.load('assets/Ground.png')
 class Player():
     def __init__(self, x, y):
         logo_surface = pygame.image.load('assets/logo.png')
-        self.image = pygame.transform.scale(logo_surface, (player_x,player_y))
+        self.image = pygame.transform.scale(logo_surface, (100,100))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        dx = 0
+        dy = 0
+
+        #get keypresses 
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+        
+        #check for collisions (later)
+
+        #update player coordinates (also later)
+        self.rect.x += dx
+        self.rect.x += dy
+
+        #draw player onto screen
+        screen.blit(self.image, self.rect)
 
 
-# logo_surface = pygame.image.load('assets/logo.png')
-player_x = 300
-player_y = 300
+
+player = Player(200,300)
 
 
-# game loop
+
+
+# ----------------- GAME LOOP -----------------
 while True:
-    # INPUT
 
-    # player input
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        player_x -= 2
-    if keys[pygame.K_d]:
-        player_x += 2
+    # PLAYER INPUT
 
-    # update
+    # --- old version (working) ---
+    #keys = pygame.key.get_pressed()
+    #if keys[pygame.K_a]:
+    #    player_x -= 2
+    #if keys[pygame.K_d]:
+    #    player_x += 2
+
+    # --- new version ---
+    #key = pygame.key.get_pressed()
+    #if key[pygame.K_LEFT]:
+    #    self.rect.x -= 5
+
+
+
+    # UPDATE
     pygame.display.update()
     clock.tick(60)
 
-    #draw
+    #DRAW
     screen.blit(background, (0,0))
     screen.blit(ground, (0,630))
-    screen.blit(logo_surface,(player_x,player_y))
+    #screen.blit(logo_surface,(player_x,player_y))
+    
+    #the cool function that does everything:
+    player.update()
 
-
-
-
-
-    #quit & frame things
+    #QUIT
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+# ----------------- GAME LOOP END -----------------
     
 
 
