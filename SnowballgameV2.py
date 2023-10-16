5292e5520ef2024b1b723555e76e6dea4a5f103b
Leon Splitt <splittleon@gmail.com>
2023-10-16 17:29:30 +0200

    add a line

--- SnowballgameV2.py
+++ SnowballgameV2.py
@@ -17,9 +17,9 @@ while True:
     
     pygame.display.update()
 
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         pygame.quit()
-    #         exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
 
