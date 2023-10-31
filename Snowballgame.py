c1a5e3c1b91d2728b92fcf4b9baf1952d0925f9e
Leon Splitt <splittleon@gmail.com>
2023-10-31 12:42:25 +0100

    method for sidekeys

--- Snowballgame.py
+++ Snowballgame.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame import mixer
 from pygame.locals import *
+from pygame.key import ScancodeWrapper
 from sys import exit
 
 # constant variables
-SCREEN_SIZE = (1080,720)
+SCREEN_SIZE = (1080, 720)
 FLOOR_HEIGHT = 635
 SPAWN_POS_X = 300
 SPAWN_POS_Y = 300
@@ -12,7 +13,7 @@ JUMP_VEL = -80
 SIDE_VEL = 20
 
 # init
-mixer.pre_init(44100, -16,2, 512)
+mixer.pre_init(44100, -16, 2, 512)
 mixer.init()
 pygame.init()
 
@@ -22,20 +23,21 @@ clock = pygame.time.Clock()
 
 
 # load assets
-background = pygame.image.load('assets/Background.png')
-ground = pygame.image.load('assets/Ground.png')
-snowball_img = pygame.image.load('assets/logo.png')
+background = pygame.image.load("assets/Background.png")
+ground = pygame.image.load("assets/Ground.png")
+snowball_img = pygame.image.load("assets/logo.png")
 
 # load sounds
-pygame.mixer.music.load('assets/night.mp3')
+pygame.mixer.music.load("assets/night.mp3")
 pygame.mixer.music.play(-1, 0.0, 0)
-jump_fx = pygame.mixer.Sound('assets/jump2.mp3')
+jump_fx = pygame.mixer.Sound("assets/jump2.mp3")
 jump_fx.set_volume(0.1)
 
+
 # player
-class Player():
+class Player:
     def __init__(self, x, y, snowball_img):
-        self.image = pygame.transform.scale((snowball_img), (40,40))
+        self.image = pygame.transform.scale((snowball_img), (40, 40))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -45,37 +47,36 @@ class Player():
 
     def is_not_jumping(self):
         return self.rect.bottom == FLOOR_HEIGHT
-    
+
     def grow_snowball(self):
         self.rect.width += 1
         self.rect.height += 1
-        self.image = pygame.transform.scale((snowball_img), (self.rect.width,self.rect.height))
+        self.image = pygame.transform.scale(
+            (snowball_img), (self.rect.width, self.rect.height)
+        )
+
+    def handle_sidekey(self, key: ScancodeWrapper, which_key: int, dir: int):
+        if key[which_key] and self.moved == False:
+            self.vel_x = dir * SIDE_VEL
+            self.moved = True
+            self.grow_snowball()
+        elif key[which_key] == False:
+            self.moved = False
 
     def update(self):
         dx = 0
         dy = 0
 
-        # handle keypresses 
+        # handle keypresses
         key = pygame.key.get_pressed()
 
         if key[pygame.K_UP] and self.is_not_jumping():
             jump_fx.play()
             self.vel_y = JUMP_VEL
 
-        if key[pygame.K_LEFT] and self.moved == False:
-            self.vel_x = SIDE_VEL
-            self.moved = True
-            self.grow_snowball()
-        elif key[pygame.K_LEFT] == False:
-            self.moved = False
-       
-        if key[pygame.K_RIGHT] and self.moved == False:
-            self.vel_x = -SIDE_VEL
-            self.moved = True
-            self.grow_snowball()
-        elif key[pygame.K_RIGHT] == False:
-            self.moved = False
-        
+        self.handle_sidekey(key, pygame.K_LEFT, 1)
+        self.handle_sidekey(key, pygame.K_RIGHT, -1)
+
         # add gravity
         self.vel_y += 10
         if self.vel_y > 10:
@@ -105,49 +106,45 @@ class Player():
         if self.rect.bottom > FLOOR_HEIGHT:
             self.rect.bottom = FLOOR_HEIGHT
             dy = 0
-    
+
     def blit(self):
-        #draw player onto screen
+        # draw player onto screen
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
 
-
-#def freeze(current_player: Player, static_list):
+# def freeze(current_player: Player, static_list):
 #    static_list.append(current_player)
 #    current_player = Player(SPAWN_POS_X,SPAWN_POS_Y, snowball_img)
 
 
-        
-#static_list = []
+# static_list = []
 clock.tick(60)
-player = Player(SPAWN_POS_X,SPAWN_POS_Y, snowball_img)
+player = Player(SPAWN_POS_X, SPAWN_POS_Y, snowball_img)
 
 
 # ----------------- GAME LOOP -----------------
 while True:
-
     # UPDATE
     pygame.display.update()
-    
-    #DRAW
-    screen.blit(background, (0,0))
-    screen.blit(ground, (0,630))
-    #for i in static_list:
-    #    i.blit()
-    #screen.blit(logo_surface,(player_x,player_y))
 
-    #the cool function that does everything:
-    
+    # DRAW
+    screen.blit(background, (0, 0))
+    screen.blit(ground, (0, 630))
+    # for i in static_list:
+    #    i.blit()
+    # screen.blit(logo_surface,(player_x,player_y))
+
+    # the cool function that does everything:
+
     player.update()
     player.blit()
     # freeze with down key
-    #key = pygame.key.get_pressed()
-    #if key[pygame.K_DOWN]:
+    # key = pygame.key.get_pressed()
+    # if key[pygame.K_DOWN]:
     #    freeze(player, static_list)
 
-
-    #QUIT
+    # QUIT
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
